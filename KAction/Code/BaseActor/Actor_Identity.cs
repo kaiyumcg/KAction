013b64bb8b898ca91c2ca4ad5f11f683f192a566
kaiyumPC <kaiyumcg@gmail.com>
2022-07-04 10:40:20 +0600

    actor identity work

--- KAction/Code/BaseActor/Actor_Identity.cs
+++ KAction/Code/BaseActor/Actor_Identity.cs
@@ -10,7 +10,33 @@ namespace GameplayFramework
         [SerializeField] List<GameplayTag> tags = null;
         public bool IsPlayer { get { return isPlayer; } }
 
-        public bool IsSimilarTo(Actor actor, bool useHierarchy = false)
+        //Bird is similar to Dinosour? 
+        //White peacock is similar to Green peacock?
+        //Fish is similar to Blue fin tuna?
+        //Blue fin tuna is similar to Fish?
+        //Cow is similar to Wagyu?
+        //Wagyu is similar to Sahiwal?
+        //Wagyu is similar to Cow?
+
+        public bool HasCommonAncestor(Actor actor)
+        {
+            bool found = false;
+            if (tags.Count > 0)
+            {
+                for (int i = 0; i < tags.Count; i++)
+                {
+                    var t = tags[i];
+                    if (actor.tags.Contains(t))
+                    {
+                        found = true;
+                        break;
+                    }
+                }
+            }
+            return found;
+        }
+        //similarly has child or has parent or immediate parent etc methods
+        public bool HasMatchedTag(Actor actor)
         {
             bool found = false;
             if (tags.Count > 0)
@@ -30,8 +56,8 @@ namespace GameplayFramework
 
         public void AddUniqueTag(GameplayTag tag)
         {
-            if (tags == null) { tags = new List<GameplayTag>(); }
             if (tag == null) { return; }
+            if (tags == null) { tags = new List<GameplayTag>(); }
             if (tags.Contains(tag) == false) { tags.Add(tag); }
         }
     }
